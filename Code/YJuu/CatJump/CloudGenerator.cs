153b8466e46258ad37cc73736e6ca3e5664e720a
YJuu <yj5048@naver.com>
2019-10-11 18:27:28 +0900

    CatJump

---
+++ Code/YJuu/CatJump/CloudGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudGenerator : MonoBehaviour {
+
+
+    public GameObject cloudPrefab;
+    float span = 1.0f;
+    float delta = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.delta += Time.deltaTime;
+        if (this.delta > this.span)
+        {
+            this.delta = 0;
+            GameObject go = Instantiate(cloudPrefab) as GameObject;
+            int px = Random.Range(-9, 9);
+            int py = Random.Range(-6, 7);
+            go.transform.position = new Vector3(px, py, 0);
+        }
+	}
+}
---
+++ Code/YJuu/CatJump/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+    Rigidbody2D rigid2D;
+    float jumpForce = 780.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)&&Mathf.Abs(this.rigid2D.velocity.y)<0.0001)
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        if(speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        if(key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+    }
+}
